4a0cf8ae9f32229e83c42ef61c73b50933a62e50
smo_ozyyy <smoozydearth@yandex.ru>
2026-08-31 21:07:48 +0700

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Антон on 31.08.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
